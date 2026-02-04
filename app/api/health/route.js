315ac464e9bc8dbadcfe6d8a0a39f60bd9d07e5b
thelastgiftteam <thelastgiftteam@gmail.com>
2026-02-04 14:45:22 +0530

    Create route.js

---
+++ app/api/health/route.js
@@ -0,0 +1,8 @@
+export async function GET() {
+  return new Response(
+    JSON.stringify({ status: "ok", message: "API is live" }),
+    {
+      headers: { "Content-Type": "application/json" }
+    }
+  );
+}
